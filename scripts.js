bdfb8e3a5bf7a74725b1d5b8f65dad2ccee97596
chltrn <chltrn@users.noreply.github.com>
2020-12-24 12:57:01 -0500

    add json

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let colorArr = [
+  '#91A6FF',
+  '#FFEEDB',
+  '#4C3B4D',
+  '#A53860',
+  '#61C9A8'
+];
+//function to change color of page on "get quote button click"
+// color changes based on an array of colors
+function changeColor(){
+  let newColor = colorArr[Math.floor(colorArr.length * Math.random())];
+  document.documentElement.style.setProperty('--mainColor', newColor);
+}
+
+
+
+$(document).ready(function() {
+    // all custom jQuery will go here
+    changeColor();
+    $(".btn").on("click",changeColor);
+
+});
