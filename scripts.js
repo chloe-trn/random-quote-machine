0fba40a7317c7c703fa5178cbeec2398fea16017
chltrn <chltrn@users.noreply.github.com>
2020-12-24 22:00:58 -0500

    add fade to background image

--- scripts.js
+++ scripts.js
@@ -1,24 +1,21 @@
 let newQuote = '';
 let newAuthor = '';
 let imgArr =[
-  'url("https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-anand-dandekar-1532771.jpg")',
-  'url("https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-blaque-x-863963.jpg")',
-  'url("https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-david-besh-884788.jpg")',
-  'url("https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-darwis-alwan-1212045.jpg")',
-  'url("https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-eberhard-grossgasteiger-454880.jpg")',
-  'url("https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-min-an-1454794.jpg")',
-  'url("https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-nicole-avagliano-2236713.jpg")',
-  'url("https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-pixabay-372166.jpg")',
-  'url("https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-simon-matzinger-1323550.jpg")'
+  'https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-anand-dandekar-1532771.jpg',
+  'https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-david-besh-884788.jpg',
+  'https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-min-an-1454794.jpg',
+  'https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-pixabay-372166.jpg'
 ];
 //function that chooses random background image:
 function changeImg(){
   let newImg = imgArr[Math.floor(imgArr.length * Math.random())];
-  document.documentElement.style.setProperty('--url',newImg);
+  $("#bgd-img").fadeOut(500, function() {
+        $("#bgd-img").attr("src",newImg);
+    }).fadeIn(900);
 }
 //function that chooses random quote from JSON file:
 function getInfo(){
-  console.log("iam in the getInfo");
+  console.log("i am in the getInfo");
   $.ajax({
     url:'https://gist.githubusercontent.com/chltrn/8dc0865ef5deedc3c33976f0bf9c103f/raw/7f0d96cb67429ce5613f24372b774fe78b3209d2/quotes.js',
     dataType: 'json',
@@ -36,12 +33,12 @@ function buttonClicked(){
   getInfo();
   changeImg();
   $(".fade").fadeOut(900,function() {
-    $("#quote-text").text(newQuote).fadeIn(1500);
-    $("#quote-author").text("-"+newAuthor).fadeIn(1500);
-    $("#quote-icon").fadeIn(1500);
+    $("#quote-text").text(newQuote).fadeIn(1600);
+    $("#quote-author").text("-"+newAuthor).fadeIn(1600);
+    $("#quote-icon").fadeIn(1600);
   });
 }
 $(document).ready(function() {
-    buttonClicked();
+    //buttonClicked();
     $(".btn").on("click",buttonClicked);
 });
