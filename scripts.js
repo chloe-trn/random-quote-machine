5d26d3a2e4f06649c19ad15096b517a3e7808788
chltrn <chltrn@users.noreply.github.com>
2020-12-30 11:26:41 -0500

    resize background image

--- scripts.js
+++ scripts.js
@@ -2,11 +2,20 @@
 let newQuote = '';
 let newAuthor = '';
 let newImg = '';
+
+function openInfoWindow(){
+   $(".info-popup").css("visibility", "visible");
+   $(".close-btn").css("display", "inline");
+}
+function closeInfoWindow(){
+  $(".info-popup").css("visibility", "hidden");
+  $(".close-btn").css("display", "none");
+}
 //function that chooses random background image and fades the new image in:
 function changeImg(){
-  $("#bgd-img").fadeOut(1500, function() {
+  $("#bgd-img").fadeOut(500, function() {
         $("#bgd-img").attr("src",newImg);
-    }).fadeIn(5500);
+    }).fadeIn(3500);
 }
 //function to set twitter intent link:
 function setTwitter(){
@@ -14,9 +23,10 @@ function setTwitter(){
    "https://twitter.com/intent/tweet?text=" + encodeURI('"'+newQuote+'"'+' -'+newAuthor));
 }
 // function that chooses random large landscape image from Unsplash API:
-async function getImg(){
+function getImg(){
   fetch("https://api.unsplash.com/photos/random/?client_id=JtXB13VTCyZyC4QFPj2cWBpgkD4TwRfMMXbdWCENMaI&orientation=landscape&content_filter=high")
   .then(function(response){
+    console.log(response.json());
     return response.json()
   })
   .then(function(data){
@@ -33,15 +43,24 @@ async function getInfo() {            //async functions return a promise
 }
 //function that changes quote, author, and background image on button click:
 function buttonClicked(){
-  getImg();
-  changeImg();
+  // check if info-window is open, if so close it
+  
   getInfo();
+  getImg();
+  //changeImg();
   $(".fade").fadeOut(900,function() {  //animation --> callback to new Quote
     $("#quote-text").text(newQuote).fadeIn(1600);
     $("#quote-author").text("-"+newAuthor).fadeIn(1600);
     $(".quote-icon").fadeIn(1600);
     setTwitter();
   });
+  $(".img-container > img").first().fadeOut(
+        function(){$(this).remove();}
+  );
+  $(".img-container").append(
+          $("#bgd-img").attr("src", newImg)
+          .fadeIn()
+  );
 }
 //executes after document is done loading:
 $(document).ready(function() {
