bec9b194353f2ca1cb26e434a32371e941149649
chltrn <chltrn@users.noreply.github.com>
2021-01-04 17:18:23 -0500

    Add server client error catch

--- scripts.js
+++ scripts.js
@@ -1,72 +1,76 @@
-//global variables:
+// global variables:
 let newQuote = '';
 let newAuthor = '';
 let newImg = "";
-//let newImg = "https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-min-an-1454794.jpg"; // TODO- get rid of this
+let quoteFetchStatus = false;
+let defaultImg = "background.jpg";
 
-// function that opens the info window:
+// open the info window:
 function openInfoWindow(){
    $(".info-popup").css("visibility", "visible");
    $(".close-btn").css("display", "inline");
 }
-// function that closes the info window:
+// close the info window:
 function closeInfoWindow(){
   $(".info-popup").css("visibility", "hidden");
   $(".close-btn").css("display", "none");
 }
-//function to set twitter intent link:
+// set twitter intent link:
 function setTwitter(){
   $("#twitter-icon").attr("href",
    "https://twitter.com/intent/tweet?text=" + encodeURI('"'+newQuote+'"'+' -'+newAuthor));
 }
-
-
-//function that fades out image fades the new image in:
+// fade out background image and fade the new image in:
 function changeImg(){
   $('body').fadeTo('slow', 0.2, function(){
     $(this).css("background-image",'url(' + newImg + ')');
   }).fadeTo('slow', 1);
 }
-// function that chooses random large landscape image from Unsplash API:
+// fetch random large landscape image from Unsplash API:
 async function getImg() {
-  //const response = await fetch("https://api.unsplash.com/photos/random/?client_id=JtXB13VTCyZyC4QFPj2cWBpgkD4TwRfMMXbdWCENMaI&orientation=landscape&content_filter=high");
-  const response = await fetch("https://api.unsplash.com/photos/random/?client_id=JtXB13VTCyZyC4QFPjafwcWBpgkD4TwRfMMXbdWCENMaI&orientation=landscape&content_filter=high");
-  console.log(response);
+  const response = await fetch("https://api.unsplash.com/photos/random/?client_id=JtXB13VTCyZyC4QFPj2cWBpgkD4TwRfMMXbdWCENMaI&orientation=landscape&content_filter=high");
   if(response.status != 200){ // error on client or server side
-    console.log("ERROR");
-    // set background pic to default image
-    newImg = "https://raw.githubusercontent.com/chltrn/randomQuoteMachine/main/imgs/pexels-min-an-1454794.jpg";
+    newImg = defaultImg ; // set background pic to default image
   }else{ // successful fetch
     const data = await response.json(); //await waits until promise settless and then executes after.
-    console.log(data);
-    console.log(data.urls.regular);
-    newImg = data.urls.raw + '&w=1500&h=750';
+    newImg = data.urls.raw + '&w=1500&h=750'; // get the random image url and change it to have width = 1500
   }
   changeImg();
-
 }
-//function that chooses random quote with author from Quotable API:
-async function getInfo() {            //async functions return a promise
-  const response = await fetch("https://api.quotable.io/random?maxlen=130");
-  const data = await response.json(); //await waits until promise settless and then executes after.
-  newQuote = data.content;
-  newAuthor = data.author;
+// fetch random quote with author from Quotable API:
+async function getInfo() {
+  const response = await fetch("https://api.quotable.io/random?maxlen=130"); // get quote with max length of 130
+  if(response.status != 200){
+    quoteFetchStatus = false;
+    alert("Error with Quotable API request; please try again.");
+  }else{
+    const data = await response.json();
+    // make sure quote is not empty and again if less than max length
+    if(data.length > 0 && data.length < 130){
+      quoteFetchStatus = true;
+      newQuote = data.content;
+      newAuthor = data.author;
+      // transition to new quote
+      $(".fade").fadeOut(500,function() {  // animation --> callback to new Quote
+        $("#quote-text").text(newQuote).fadeIn(1400);
+        $("#quote-author").text("-"+newAuthor).fadeIn(1400);
+        $(".quote-icon").fadeIn(1400);
+        setTwitter();
+      });
+    }else{
+      getInfo(); // fetch again
+    }
+  }
 }
-//function that changes quote, author, and background image on button click:
+// change quote, author, and background image on button click:
 function buttonClicked(){
   // check if info-window is visible:
   if( $(".info-popup").css('visibility').toLowerCase() == 'visible') {
     closeInfoWindow(); //close it
   }
   getInfo();
-  $(".fade").fadeOut(500,function() {  //animation --> callback to new Quote
-    $("#quote-text").text(newQuote).fadeIn(1400);
-    $("#quote-author").text("-"+newAuthor).fadeIn(1400);
-    $(".quote-icon").fadeIn(1400);
-    setTwitter();
-  });
 }
-//executes after document is done loading:
+//execute after document is done loading:
 $(document).ready(function() {
     $(".quote-btn").on("click",buttonClicked);  // listen for button click event
     $(".image-btn").on("click",getImg);
